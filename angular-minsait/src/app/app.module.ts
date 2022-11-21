17a63398526aff6b715643aaeabc9194629ac6f0
Pau March <pmarch@minsait.com>
2022-11-21 10:08:43 +0100

    CRUD Completed

--- angular-minsait/src/app/app.module.ts
+++ angular-minsait/src/app/app.module.ts
@@ -18,6 +18,13 @@ import {MatRadioModule} from '@angular/material/radio';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+
+
+
+
 
 
 @NgModule({
@@ -39,7 +46,10 @@ import { HttpClientModule } from '@angular/common/http';
     MatRadioModule,
     ReactiveFormsModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-minsait/src/app/dialog/dialog.component.ts
+++ angular-minsait/src/app/dialog/dialog.component.ts
@@ -1,7 +1,9 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ApiService } from '../services/api.service';
+
+import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog'
 
 @Component({
   selector: 'app-dialog',
@@ -12,8 +14,12 @@ export class DialogComponent implements OnInit {
 
   freshnessList = ["Brand New", "Second Hand", "Refurbished"];
   productForm !: FormGroup;
+  actionBtn : string = "Save";
 
-  constructor(private formBuilder : FormBuilder, private api : ApiService) { }
+  constructor(private formBuilder : FormBuilder, 
+    private api : ApiService, 
+    @Inject(MAT_DIALOG_DATA) public editData : any,
+    private dialogRef : MatDialogRef<DialogComponent>) { }
 
   ngOnInit(): void {
     this.productForm = this.formBuilder.group({
@@ -23,19 +29,45 @@ export class DialogComponent implements OnInit {
       origin : ['',Validators.required],
 
     })
+    if(this.editData){
+      this.actionBtn = "Update";
+      this.productForm.controls['productName'].setValue(this.editData.productName)
+      this.productForm.controls['species'].setValue(this.editData.species)
+      this.productForm.controls['freshness'].setValue(this.editData.freshness)
+      this.productForm.controls['origin'].setValue(this.editData.origin)
+    }
   }
   addProduct(){
-    if(this.productForm.valid){
-      this.api.postProduct(this.productForm.value)
-      .subscribe({
-        next:(res)=>{
-          alert("Product added succesfully")
-        },
-        error:()=>{
-          alert("Error while adding the product")
-        }
-      })
-    }
+    if(this.editData){
+      if(this.productForm.valid){
+        this.api.postProduct(this.productForm.value)
+        .subscribe({
+          next:(res)=>{
+            alert("Product added succesfully")
+            this.productForm.reset();
+            this.dialogRef.close('save');
+          },
+          error:()=>{
+            alert("Error while adding the product")
+          }
+        })
+      }
+    }else{
+      this.updateProduct()
+  }
+}
+  updateProduct(){
+    this.api.putProduct(this.productForm.value,this.editData.id)
+    .subscribe({
+      next:(res)=>{
+        alert("Product updated succesfully.")
+        this.productForm.reset();
+        this.dialogRef.close('update');
+      },
+      error:()=>{
+        alert("Error while updating the record.")
+      }
+    })
   }
 
 }
